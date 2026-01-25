14616ba85cedbcb6c9b44af71fb3f44c61b1859a
InsideDynamic <vn@insidedynamic.de>
2026-01-25 21:37:58 +0000

    Add directory creation for FreeSWITCH SIP profiles and dialplan in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+# Создать директории, если их нет
+mkdir -p /etc/freeswitch/sip_profiles
+mkdir -p /etc/freeswitch/dialplan
+
 # Копируем шаблоны как есть
 cp /templates/internal.xml.tpl /etc/freeswitch/sip_profiles/internal.xml
 cp /templates/external.xml.tpl /etc/freeswitch/sip_profiles/external.xml
